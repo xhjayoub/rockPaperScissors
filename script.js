1c28750faafe1c4a81d3620180220e0d3ce9a812
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-13 11:52:18 +0100

    Add getHumanChoice function
    Taking user input and return it.

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function getComputerChoice() {
     let nr = Math.floor(Math.random()*3);
     return (nr === 0) ? "rock" : (nr === 1) ? "paper" : "scissors";
 }
+
+// Function getHumanChoice
+// Take user input and return it
+function getHumanChoice() {
+    let userChoice = prompt("Choose 'rock','paper' or 'scissors'");
+    
+    return (userChoice === "rock") ? "rock" : (userChoice === "paper") ? "paper": (userChoice === "scissors") ? "scissors" : undefined;
+}
