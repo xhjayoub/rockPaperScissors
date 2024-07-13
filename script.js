255ef4c0612f4e04703c4a72db1547056e7d4ad6
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-13 12:29:46 +0100

    Format the script.js document

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
-// Function getComputerChoice 
+// Function getComputerChoice
 // Randomly return "rock" ,"paper" or "scissors"
 
 function getComputerChoice() {
-    let nr = Math.floor(Math.random()*3);
-    return (nr === 0) ? "rock" : (nr === 1) ? "paper" : "scissors";
+  let nr = Math.floor(Math.random() * 3);
+  return nr === 0 ? "rock" : nr === 1 ? "paper" : "scissors";
 }
 
 // Function getHumanChoice
 // Take user input and return it
 function getHumanChoice() {
-    let userChoice = prompt("Choose 'rock','paper' or 'scissors'");
-    
-    return (userChoice.toLowerCase() === "rock") ? "rock" : (userChoice.toLowerCase() === "paper") ? "paper": (userChoice.toLowerCase() === "scissors") ? "scissors" : getHumanChoice();
+  let userChoice = prompt("Choose 'rock','paper' or 'scissors'");
+
+  return userChoice.toLowerCase() === "rock"
+    ? "rock"
+    : userChoice.toLowerCase() === "paper"
+    ? "paper"
+    : userChoice.toLowerCase() === "scissors"
+    ? "scissors"
+    : getHumanChoice();
 }
 
 // Function playRound
 // Play a round and increment winner score
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log("No winner.");
+  if (humanChoice === computerChoice) {
+    console.log("No winner.");
+  } else {
+    if (
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      humanScore++;
+      console.log("You are the winner of the round");
     } else {
-        if (humanChoice === "paper" && computerChoice === "rock" || humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "paper") {
-            humanScore++;
-            console.log("You are the winner of the round");
-        } else {
-            computerScore++;
-            console.log("You lose! " + computerChoice + " beats " + humanChoice);
-        }
+      computerScore++;
+      console.log("You lose! " + computerChoice + " beats " + humanChoice);
     }
+  }
 }
-
 
 // Scores
 let humanScore = 0;
