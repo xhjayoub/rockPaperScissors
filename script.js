5eb0c40d6cf7577994f1885e9df5eb25f49b7f84
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-13 11:42:18 +0100

    Add getComputerChoice function
    Generating randomly one of the following
    string values: "rock","paper" or "scissors".

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+// Function getComputerChoice 
+// Randomly return "rock" ,"paper" or "scissors"
+
+function getComputerChoice() {
+    let nr = Math.floor(Math.random()*3);
+    return (nr === 0) ? "rock" : (nr === 1) ? "paper" : "scissors";
+}
